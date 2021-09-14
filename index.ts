d14e05d931a12a6c0891199967fecfa69c644c78
Finn Pauls <mbp@pauls.dev>
2021-09-14 13:57:09 +0200

    Slight refactor

--- index.ts
+++ index.ts
@@ -1,46 +1,15 @@
+import { countTokens, getSubtitles, getTextFromSubtitle, hasALetter, isNotStopword } from "./src/lib.ts";
 
+const subtitles = await getSubtitles();
 
-const request = (await fetch('https://raw.githubusercontent.com/stopwords-iso/stopwords-pt/master/stopwords-pt.json'))
-const stopwords = await request.json()
+const allLines = subtitles.map(getTextFromSubtitle).flat()
 
-const subtitles = []
-
-for await (const dirEntry of Deno.readDir('./subtitles/pt')) {
-    subtitles.push(await Deno.readTextFile('./subtitles/pt/' + dirEntry.name))
-}
-
-const text = subtitles.join('\n')
-
-const completeText = text.split('\n\n').map(s => s.split('\n').slice(2)).flat().join(' ');
-
-const tokens = completeText.toLowerCase().split(/\s+/)
+const tokens = allLines.join('\n').toLowerCase().replace(/\,/g, '').split(/\s+/)
 
 const cleanedTokens = tokens.filter(isNotStopword).filter(hasALetter)
 
-const tokenCounts = countTokens(cleanedTokens)
+const tokensSorted = countTokens(cleanedTokens)
 
-const tokensSorted = Object.entries(tokenCounts).sort((a, b) => b[1] - a[1])
+console.log(tokensSorted.slice(0, 40))
 
-console.log(tokensSorted.slice(0, 20))
 
-function isNotStopword(token: string) {
-    return !stopwords.includes(token)
-}
-
-function hasALetter(token: string) {
-    return /[a-zA-Z]/.test(token)
-}
-
-function countTokens(tokens: string[]) {
-    const tokenCounts: {[key: string]: number} = {}
-
-    for (const token of tokens) {
-        if (!tokenCounts[token]) {
-            tokenCounts[token] = 1
-        } else {
-            tokenCounts[token] += 1
-        }
-    }
-
-    return tokenCounts
-}
---
+++ src/lib.ts
@@ -0,0 +1,38 @@
+const request = (await fetch('https://raw.githubusercontent.com/stopwords-iso/stopwords-pt/master/stopwords-pt.json'))
+const stopwords = await request.json()
+
+export function isNotStopword(token: string) {
+    return !stopwords.includes(token)
+}
+
+export function hasALetter(token: string) {
+    return /[a-zA-Z]/.test(token)
+}
+
+export function countTokens(tokens: string[]) {
+    const tokenCounts: {[key: string]: number} = {}
+
+    for (const token of tokens) {
+        if (!tokenCounts[token]) {
+            tokenCounts[token] = 1
+        } else {
+            tokenCounts[token] += 1
+        }
+    }
+
+    return Object.entries(tokenCounts).sort((a, b) => b[1] - a[1])
+}
+
+export async function getSubtitles() {
+    const subtitles = []
+
+    for await (const dirEntry of Deno.readDir('./subtitles/pt')) {
+        subtitles.push(await Deno.readTextFile('./subtitles/pt/' + dirEntry.name))
+    }
+
+    return subtitles
+} 
+
+export function getTextFromSubtitle(subtitle: string): string[] {
+    return subtitle.split('\n\n').map(s => s.split('\n').slice(2).join('\n'))
+ }
